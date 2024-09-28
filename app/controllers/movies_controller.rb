0bc3cbb2163581d0f5b37afaeab0f7429e43dca7
Anna Vi11arrea1 <stayfluorescent@gmail.com>
2024-09-28 18:00:06 +0000

    working on movie details

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   def show_movies
-    render({ :template => "misc_templates/movies"})
+    render({ :template => "movie_templates/movies"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Movie.where({ :id => the_id })
+    @the_movie = matching_records.at(0)
+    render({ :template => "movie_templates/details"})
   end
 end
